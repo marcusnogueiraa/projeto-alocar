35839d905e84713293e7219e3dc31a8b1ef4cebd
Marcus Nogueira <112811596+marcusnogueiraa@users.noreply.github.com>
2022-10-26 22:49:32 -0300

    Add files via upload
    Versão 1.0 do Projeto Alocar.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tudo.h"
+int main(void){
+    ler_dados();
+    logo();
+    menu_geral();
+    return 0;
+}
